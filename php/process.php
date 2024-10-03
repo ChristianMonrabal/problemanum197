19d8770be9f90e6be483476a542bbed7766f4cbd
Christian <christianmonrabaldonis@gmail.com>
2024-10-03 19:31:49 +0200

    PHP code

---
+++ php/process.php
@@ -0,0 +1,43 @@
+<?php
+
+// Función para desencriptar X'' a X'
+function decryptStageXII($text) {
+    $leftPart = '';
+    $rightPart = '';
+
+    // Dividir el texto en dos partes
+    for ($i = 0; $i < strlen($text); $i++) {
+        ($i % 2 === 0) ? $leftPart .= $text[$i] : $rightPart = $text[$i] . $rightPart;
+    }
+
+    return $leftPart . $rightPart;
+}
+
+// Función para desencriptar de X' a X
+function transformTextX($text) {
+    $vowels = ['a', 'e', 'i', 'o', 'u'];
+    $result = '';
+    $buffer = '';
+
+    for ($i = 0; $i < strlen($text); $i++) {
+        $char = $text[$i];
+        if (in_array(strtolower($char), $vowels)) {
+            $result .= strrev($buffer) . $char;
+            $buffer = '';
+        } else {
+            $buffer .= $char;
+        }
+    }
+
+    return $result . strrev($buffer);
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && $_POST['action'] === 'decrypt') {
+    $encryptedXII = $_POST['text-desencriptar'];
+    $decryptedXI = decryptStageXII($encryptedXII);
+    $plainText = transformTextX($decryptedXI);
+    
+    header("Location: ../index.php?decryptedXI=" . urlencode($decryptedXI) . "&decryptedText=" . urlencode($plainText));
+    exit();
+}
+?>
